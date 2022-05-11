4c7a7ce890ce0692a594ac5222e7931595a31889
Duwareon <duwareon@gmail.com>
2022-05-11 09:42:46 -0400

    changed file structure and code layout, implemented ADC #

---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+pub trait Bit {
+    fn get_bit(&self, bit: u8) -> bool;
+    fn set_bit(&mut self, bit: u8) -> Self;
+    fn unset_bit(&mut self, bit: u8) -> Self;
+}
+
+impl Bit for u8 {
+    fn get_bit(&self, bit: u8) -> bool {
+        return ((self >> bit) & 1) != 0
+    }
+    fn set_bit(&mut self, bit: u8) -> Self {
+        return *self | 0b00000001 << bit
+    }
+    fn unset_bit(&mut self, bit: u8) -> Self {
+        return *self & 0b11111110 << bit
+    }
+}
+
+impl Bit for u16 {
+    fn get_bit(&self, bit: u8) -> bool {
+        return ((self >> bit) & 1) != 0
+    }
+    fn set_bit(&mut self, bit: u8) -> Self {
+        return *self | 0b0000000000000001 << bit
+    }
+    fn unset_bit(&mut self, bit: u8) -> Self {
+        return *self & 0b1111111111111110 << bit
+    }
+}
+
+pub struct CPU {
+    pc: u16, //Program Counter, should look at $FFFC and $FFFD for start location
+    sp: u8,  //Stack Pointer, inits to $FD and gets 100 added to its value when used
+    sr: u8,  //Status Register, each bit is a flag, NV-BDIZC
+
+    a: u8, //Registers
+    x: u8,
+    y: u8,
+}
+
+impl CPU {
+    pub fn reset(&mut self, mem: &mut MEM) {
+        self.pc = mem.get(0xFFFC) as u16 + (mem.get(0xFFFD) as u16)*0x100;
+        self.sp = 0xFD;
+        self.sr = 0b00100000;
+        self.a = 0;
+        self.x = 0;
+        self.y = 0;
+    }
+
+    pub fn new( mem: &mut MEM) -> CPU {
+        let mut newcpu = CPU {pc: 0, sp: 0, sr: 0, a: 0, x: 0, y: 0};
+        newcpu.reset(mem);
+        return newcpu;
+    }
+
+    fn get_sp(&mut self, mem: &mut MEM) -> u8 {
+        let addr: u16 = (self.sp as u16) + 0x100;
+        return mem.get(addr);
+    }
+
+    fn get_next(&mut self, mem: &mut MEM) -> u8 {
+        let data = mem.get(self.pc as u16);
+        self.pc += 1;
+        return data;
+    }
+
+    pub fn exec(&mut self, mem: &mut MEM) {
+        let inst = self.get_next(mem);
+        match inst {
+            0xA9 => { //LDA #
+                let val = self.get_next(mem);
+                self.a = val;
+            }
+
+            0x69 => { //ADC #
+                let val = self.get_next(mem);
+                self.a = self.a + val;
+                if (self.a as u16 + val as u16).get_bit(8) {
+                    self.sr.set_bit(0);
+                }
+            }
+
+            0x85 => { //STA zp
+                let addr = self.get_next(mem);
+                mem.set(addr as u16, self.a);
+            }
+
+            _ => {
+                print!("BAD OPCODE!")
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct MEM {
+    ram: [u8; 65535], //$0000 to $00FF is ZP, $0100 to $01FF is stack, $0200 to $FFFF is general purpose.
+}
+
+impl MEM {
+    pub fn new() -> MEM {
+        let mut newmem = MEM {ram: [0; 0xFFFF]};
+        newmem.init();
+        return newmem;
+    }
+    pub fn init(&mut self) {
+        self.ram = [0; 0xFFFF]
+    }
+
+    pub fn get(&mut self, addr: u16) -> u8 {
+        return self.ram[addr as usize]
+    }
+
+    pub fn set(&mut self, addr: u16, val: u8) {
+        self.ram[addr as usize] = val
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,25 @@
-fn set_bit(byte: u8, bit: u8) -> u8 {
-    return byte | 0b00000001 << bit + 1;
-}
-fn unset_bit(byte: u8, bit: u8) -> u8 {
-    return byte & 0b11111110 << bit + 1;
-}
-fn get_bit(byte: u8, bit: u8) -> bool {
-    return ((byte >> bit + 1) & 1) != 0;
-}
-struct CPU {
-    pc: u16, //Program Counter, should look at $FFFC and $FFFD for start location
-    sp: u8,  //Stack Pointer, inits to $FD and gets 100 added to its value when used
-    sr: u8,  //Status Register, each bit is a flag, NV-BDIZC
-
-    a: u8, //Registers
-    x: u8,
-    y: u8,
-}
-
-impl CPU {
-    fn reset(&mut self, memory: &mut MEM) {
-        self.pc = 0xFFFC;
-        self.sp = 0xFD;
-        self.sr = 0b00100000;
-        self.a = 0;
-        self.x = 0;
-        self.y = 0;
-
-        memory.init();
-    }
-
-    fn get_sp(&mut self, mem: &mut MEM) -> u8 {
-        let addr: u16 = (self.sp as u16) + 0x100;
-        return mem.get(addr);
-    }
-
-    fn get_inst(&mut self, mem: &mut MEM) -> u8 {
-        let data = mem.get(self.pc as u16);
-        self.pc += 1;
-        return data;
-    }
-
-    fn exec(&mut self, mem: &mut MEM) {
-        let inst = self.get_inst(mem);
-        match inst {
-            0xA9 => { //LDA a
-                let val = self.get_inst(mem);
-                self.a = val;
-            }
-
-            0x85 => { //STA zp
-                let addr = self.get_inst(mem);
-                println!("{:x}", addr);
-                println!("{:x}", self.a);
-                println!("{:X}", mem.get(0xA3));
-                mem.set(addr as u16, self.a);
-                println!("{:X}", mem.get(0xA3));
-            }
-
-            _ => {
-                print!("BAD OPCODE!")
-            }
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
-struct MEM {
-    ram: [u8; 65535], //$0000 to $00FF is ZP, $0100 to $01FF is stack, $0200 to $FFFF is general purpose.
-}
-
-impl MEM {
-    fn init(&mut self) {
-        self.ram = [0; 0xFFFF];
-    }
-
-    fn get(&mut self, addr: u16) -> u8 {
-        return self.ram[addr as usize];
-    }
-
-    fn set(&mut self, addr: u16, val: u8) {
-        self.ram[addr as usize] = val;
-    }
-}
+mod lib;
+use lib::CPU;
+use lib::MEM;
 
 fn main() {
-    let mut cpu = CPU {
-        pc: 0,
-        sp: 0,
-        sr: 0,
-        a: 0,
-        x: 0,
-        y: 0,
-    };
-    let mut memory = MEM { ram: [0; 0xFFFF] };
+    let mut memory = MEM::new();
+    let mut cpu = CPU::new(&mut memory);
+
+    memory.init();
+
+    memory.set(0xFFFC, 0x00);
+    memory.set(0xFFFD, 0xFF);
 
     cpu.reset(&mut memory);
 
-    cpu.a = 0x23;
+    memory.set(0xFF00, 0xA9); //LDA #$10
+    memory.set(0xFF01, 0x10);
+    memory.set(0xFF02, 0x85); //STA $A3
+    memory.set(0xFF03, 0xA3);
 
-    memory.ram[0xFFFC] = 0x85;
-    memory.ram[0xFFFD] = 0xA3;
     cpu.exec(&mut memory);
+    cpu.exec(&mut memory);
+
     println!("{:x}", memory.get(0xA3));
-    assert!(memory.get(0xA3)==cpu.a);
 }
